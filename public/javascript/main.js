cb1f9bb28e7302fa9061b0d614b9e0d55c991396
ElexemSDdR <valendomta@gmail.com>
2025-02-18 20:28:46 -0300

    Refactoring a little bit the code

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,12 +1,32 @@
-export const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
-export const KANTO_POKEMON_NUMBER_LIMIT = 151
+const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+const REGIONS_PKMN_NUMBER_LIMITS = {
+	LIMIT_KANTO: 151,
+	LIMIT_JOHTO: 251,
+	LIMIT_HOENN: 386,
+	LIMIT_SINNOH: 493,
+	LIMIT_UNOVA: 649,
+	LIMIT_KALOS: 721
+}
 
-export const obtainPokemonData = async (limit) => {
-	const res = await fetch(`${API_URL}?limit=${limit}`)
+const REGIONS_PKMN_NUMBER_OFFSET = {
+	OFFSET_KANTO: 0,
+	OFFSET_JOHTO: 152,
+	OFFSET_HOENN: 252,
+	OFFSET_SINNOH: 387,
+	OFFSET_UNOVA: 494,
+	OFFSET_KALOS: 650
+}
+
+const { LIMIT_KANTO, LIMIT_JOHTO, LIMIT_HOENN, LIMIT_SINNOH, LIMIT_UNOVA, LIMIT_KALOS } = REGIONS_PKMN_NUMBER_LIMITS
+
+const { OFFSET_KANTO, OFFSET_JOHTO, OFFSET_HOENN, OFFSET_SINNOH, OFFSET_UNOVA, OFFSET_KALOS } = REGIONS_PKMN_NUMBER_OFFSET
+
+const obtainPokemonData = async (limit, offset = 0) => {
+	const res = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`)
 	const data = await res.json()
 	return data
 }
 
-export const kantoDex = obtainPokemonData(KANTO_POKEMON_NUMBER_LIMIT)
+export const kantoDex = obtainPokemonData(LIMIT_KANTO)
 
 //Por ahora se recupera un poco la pokedex de Kanto, los primeros 151 pokemones, quedaria crear el HTML y demas
