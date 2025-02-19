182b8fa8c8689dfa2cc4b9d455894f1a654dd382
ElexemSDdR <valendomta@gmail.com>
2025-02-19 10:14:07 -0300

    Issues with change region buttons solved, now the pokedex could be of Kanto, Johto, Hoenn, Sinnoh, Unova or Kalos

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,23 +1,53 @@
 import { 
-  kantoDex
+  API_URL,
+  kantoDex,
+  obtainPokemonData,
+  LIMIT_KANTO,
+  LIMIT_HOENN,
+  LIMIT_SINNOH,
+  LIMIT_UNOVA,
+  LIMIT_KALOS,
+  OFFSET_KANTO,
+  OFFSET_JOHTO,
+  OFFSET_HOENN,
+  OFFSET_SINNOH,
+  OFFSET_UNOVA,
+  OFFSET_KALOS,
+  LIMIT_JOHTO
 } from './main.js'
 
 const $pokedex = document.querySelector('#pokedex')
 
-const renderPokemon = async () => {
-  const pkmns = await kantoDex 
+const $kanto = document.querySelector('#kanto')
+const $johto = document.querySelector('#johto')
+const $hoenn = document.querySelector('#hoenn')
+const $sinnoh = document.querySelector('#sinnoh')
+const $unova = document.querySelector('#unova')
+const $kalos = document.querySelector('#kalos')
+
+const renderPokemon = async (regionDex = kantoDex) => {
+  const pkmns = await regionDex 
   let aux = []
-  pkmns.results.forEach(async pkmn => {
-    const { url } = await pkmn
+  await pkmns.results.forEach(async pkmn => {
+    const { url } = pkmn
     
     try {
+      aux = []
       const pkmnData = await fetch(url)
       const pkmnDataJson = await pkmnData.json()
       aux.push(pkmnDataJson)
       
-      const sortedPkmns = aux.length === 151 
-      ? aux.sort((a, b) => a.order - b.order)
-      : null 
+      let sortedPkmns = []
+      if (
+          aux.length === LIMIT_KANTO || 
+          aux.length === LIMIT_JOHTO || 
+          aux.length === LIMIT_HOENN || 
+          aux.length === LIMIT_SINNOH || 
+          aux.length === LIMIT_UNOVA || 
+          aux.length === LIMIT_KALOS
+        ) {
+        sortedPkmns = aux.sort((a, b) => a.id - b.id)
+      }
 
       $pokedex.innerHTML = ''
       sortedPkmns.forEach(sortPkmn => {
@@ -30,10 +60,42 @@ const renderPokemon = async () => {
           </div>
         `      
       })
+      console.log(aux)
     } catch (error) {
       $pokedex.innerHTML = `<div class="loader"></div>`
     }
   })
 }
 
-await renderPokemon()
+$kanto.addEventListener('click', async () => {
+  await renderPokemon()
+})
+
+$johto.addEventListener('click', async () => {
+  const johtoPkmns = await obtainPokemonData(LIMIT_JOHTO, OFFSET_JOHTO)
+  await renderPokemon(johtoPkmns)
+})
+
+$hoenn.addEventListener('click', async () => {
+  const hoennPkmns = await obtainPokemonData(LIMIT_HOENN, OFFSET_HOENN)
+  await renderPokemon(hoennPkmns)
+})
+
+$sinnoh.addEventListener('click', async () => {
+  const sinnohPkmns = await obtainPokemonData(LIMIT_SINNOH, OFFSET_SINNOH)
+  await renderPokemon(sinnohPkmns)
+})
+
+$unova.addEventListener('click', async () => {
+  const unovaPkmns = await obtainPokemonData(LIMIT_UNOVA, OFFSET_UNOVA)
+  await renderPokemon(unovaPkmns)
+})
+
+$kalos.addEventListener('click', async () => {
+  const kalosPkmns = await obtainPokemonData(LIMIT_KALOS, OFFSET_KALOS)
+  await renderPokemon(kalosPkmns)
+})
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await renderPokemon()
+})
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,32 +1,32 @@
-const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+export const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 const REGIONS_PKMN_NUMBER_LIMITS = {
 	LIMIT_KANTO: 151,
-	LIMIT_JOHTO: 251,
-	LIMIT_HOENN: 386,
-	LIMIT_SINNOH: 493,
-	LIMIT_UNOVA: 649,
-	LIMIT_KALOS: 721
+	LIMIT_JOHTO: 100,
+	LIMIT_HOENN: 135,
+	LIMIT_SINNOH: 107,
+	LIMIT_UNOVA: 156,
+	LIMIT_KALOS: 72
 }
 
 const REGIONS_PKMN_NUMBER_OFFSET = {
 	OFFSET_KANTO: 0,
-	OFFSET_JOHTO: 152,
-	OFFSET_HOENN: 252,
-	OFFSET_SINNOH: 387,
-	OFFSET_UNOVA: 494,
-	OFFSET_KALOS: 650
+	OFFSET_JOHTO: 151,
+	OFFSET_HOENN: 251,
+	OFFSET_SINNOH: 386,
+	OFFSET_UNOVA: 493,
+	OFFSET_KALOS: 649
 }
 
-const { LIMIT_KANTO, LIMIT_JOHTO, LIMIT_HOENN, LIMIT_SINNOH, LIMIT_UNOVA, LIMIT_KALOS } = REGIONS_PKMN_NUMBER_LIMITS
+export const { LIMIT_KANTO, LIMIT_JOHTO, LIMIT_HOENN, LIMIT_SINNOH, LIMIT_UNOVA, LIMIT_KALOS } = REGIONS_PKMN_NUMBER_LIMITS
 
-const { OFFSET_KANTO, OFFSET_JOHTO, OFFSET_HOENN, OFFSET_SINNOH, OFFSET_UNOVA, OFFSET_KALOS } = REGIONS_PKMN_NUMBER_OFFSET
+export const { OFFSET_KANTO, OFFSET_JOHTO, OFFSET_HOENN, OFFSET_SINNOH, OFFSET_UNOVA, OFFSET_KALOS } = REGIONS_PKMN_NUMBER_OFFSET
 
-const obtainPokemonData = async (limit, offset = 0) => {
-	const res = await fetch(`${API_URL}?limit=${limit}&offset=${offset}`)
+export const obtainPokemonData = async (limit = 151, offset = 0) => {
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`)
 	const data = await res.json()
 	return data
 }
 
-export const kantoDex = obtainPokemonData(LIMIT_KANTO)
+export const kantoDex = obtainPokemonData()
 
 //Por ahora se recupera un poco la pokedex de Kanto, los primeros 151 pokemones, quedaria crear el HTML y demas
